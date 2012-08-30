e16a0bc02e84c5263f872fe2542c92dc110e71df
junh1024 <junh1024@608c9802-503b-c744-dfa9-b218aa3dcc6d>
2012-08-30 09:30:05 +0000

    add comments, phase mode optimization, fix crash on EOF, gaps in lines when resized bigger

--- GUI.py
+++ GUI.py
@@ -74,14 +74,16 @@ def loadfile(file):
 		output = True)
 
 def play():
-	global ang,data,maxarray,phaarray,datalen,bufsize,prevmaxarray,doSFR,ballisticsmode
+	global ang,data,maxarray,phaarray,datalen,bufsize,prevmaxarray,doSFR,ballisticsmode,Playing
 	
 	L_temp=0
 	R_temp=0
 
-	ang=(ang+0)%360
-	# print ang
+	# ang=(ang+5)%360
 	data = wf.readframes(bufsize)
+	if(data==''):
+		Playing=False
+		return
 	
 	if 	wf.getnchannels() ==1:
 		datalen= len(data)/2
@@ -120,7 +122,7 @@ def play():
 			R[i]=R_temp
 			
 	
-	for i in xrange(0,datalen): #applylimiting
+	for i in xrange(0,datalen): #apply limiting so that it can be packed into short/16bit. Valid for 16bit only.
 		if L[i] >32767:
 			L[i]=32767
 		if R[i] >32767:
@@ -137,19 +139,20 @@ def play():
 	# outfft=fft(maxarray)
 	
 	Lfft=fft(Lw)#compute FFT of windowed samples
-	Lpha=angle(Lfft)#extract angle data from FFT
 	Rfft=fft(Rw)
-	Rpha=angle(Rfft)
 	
-	for i in xrange(0,datalen/2): #compute phase apply phase ballistics
-		phatemp=abs(Lpha[i]-Rpha[i])#compute phase difference
-		
-		if phatemp>(prevphaarray[i]+0.2): #fixed-decay ballistics
-			phaarray[i]=prevphaarray[i]+0.2
-		else:
-			phaarray[i]=phatemp
-		
-		prevphaarray[i]=phaarray[i]
+	if(phasetext!="none"):
+		Lpha=angle(Lfft)#extract angle data from FFT
+		Rpha=angle(Rfft)
+		for i in xrange(0,datalen/2): #compute phase apply phase ballistics
+			phatemp=abs(Lpha[i]-Rpha[i])#compute phase difference
+			
+			if phatemp>(prevphaarray[i]+0.2): #fixed-decay ballistics
+				phaarray[i]=prevphaarray[i]+0.2
+			else:
+				phaarray[i]=phatemp
+
+			prevphaarray[i]=phaarray[i]
 	
 	for i in xrange(0,datalen/2): #compute fft
 		ffttemp[i]=max ( abs(Lfft[i].real), abs(Rfft[i].real) ) #get the maximum of two channels' FFT
@@ -157,8 +160,8 @@ def play():
 			ffttemp[i]=10*log10(ffttemp[i]/float(datalen)) +40#scale by the number of points so that the magnitude does not depend on the length of FFT, he power in decibels by taking 10*log10, +40 so shouldn't be -ve values at 16bit
 		except:
 			pass
-	
-	if ballisticsmode ==  "1-way variable decay":
+	#algorithms for applying ballistics
+	if ballisticsmode ==  "1-way variable decay":#1wvd-decay increases with frequency
 		ballisticscoefficient=[0]*(datalen/2)
 		for i in xrange(0,datalen/2):
 			ballisticscoefficient[i]=1.2+(i*2/(datalen/2))
@@ -170,7 +173,7 @@ def play():
 				maxarray[i]=ffttemp[i]
 			prevmaxarray[i]=maxarray[i]
 		
-	elif ballisticsmode =="1-way fixed decay":
+	elif ballisticsmode =="1-way fixed decay":#1wfd-decay is doesn't vary with frequency
 		for i in xrange(0,datalen/2):
 			if ffttemp[i]<(prevmaxarray[i]-1.2):
 				maxarray[i]=prevmaxarray[i]-1.2
@@ -180,12 +183,12 @@ def play():
 			
 	elif ballisticsmode =="2-way average":
 		for i in xrange(0,datalen/2):
-			maxarray[i]=(prevmaxarray[i]+ffttemp[i])/2 #average with 1ref ballistics
+			maxarray[i]=(prevmaxarray[i]+ffttemp[i])/2 #average with 1 previous reference
 			prevmaxarray[i]=ffttemp[i]
 		
 	elif ballisticsmode =="infinite maximum":
 		for i in xrange(0,datalen/2):
-			if ffttemp[i]>(prevmaxarray[i]): #infinite maximum ballistics
+			if ffttemp[i]>(prevmaxarray[i]): #infinite maximum
 				maxarray[i]=ffttemp[i]
 			prevmaxarray[i]=maxarray[i]
 	else:
@@ -213,7 +216,7 @@ def play():
 	data=file_str.getvalue()
 	stream.write(data)#plays the data
 
-class ResettingSlider(QtGui.QSlider):
+class ResettingSlider(QtGui.QSlider):#if it's double clicked, it resets to a reset value defined here
 	def setRSV(self,rsv):
 		self.resetvalue=rsv
 	
@@ -226,9 +229,9 @@ class SpectrumWidget(QtGui.QWidget):
 	def paintEvent(self, e):
 		qp = QtGui.QPainter()
 		qp.begin(self)
-		# qp.setRenderHint(QtGui.QPainter.Antialiasing)
+		# qp.setRenderHint(QtGui.QPainter.Antialiasing) #slow as balls
 		self.drawBackground(qp)
-		if(wf):
+		if(wf):#if a file is loaded, scale lines are drawn
 			self.drawScale(qp)
 		self.drawLines(qp)
 		qp.end()
@@ -250,15 +253,15 @@ class SpectrumWidget(QtGui.QWidget):
 		
 		apen=QtGui.QPen()
 		apen.setColor(QtGui.QColor(111,111,111))
-		qp.setPen(apen)
+		qp.setPen(apen) #set colour for drawing scale info
 		
-		SR= wf.getframerate()
+		SR= wf.getframerate() #get the sampling rate of file
 		
-		alist=[]
+		alist=[] #make array of 2^x where x is 7 to 14, for hertz to plot scale lines
 		for i in xrange(7,(14+1)):
 			alist.append(2**(i))
 		
-		for hz in alist:
+		for hz in alist: #draw the lines, while compensating for scale slider
 			ti = (pow(hz,1/power)/pow(SR/2,1/power))*w
 			p1=QtCore.QPointF(ti,0)
 			p1a=QtCore.QPointF(ti,20)
@@ -267,18 +270,11 @@ class SpectrumWidget(QtGui.QWidget):
 			qp.drawText(p1a,str(hz) + " hz")
 
 			
-		xpos=(QtGui.QWidget.mapFromGlobal (self, QtGui.QCursor.pos()).x())
+		xpos=(QtGui.QWidget.mapFromGlobal (self, QtGui.QCursor.pos()).x()) #get cursor position relative to current widget
 		if(self.underMouse() ):
 			
 			freq= int((pow(xpos,power)/pow(w,power))*(SR/2))
-			qp.drawText( QtCore.QPointF(w-64, 40) , str(freq)+" hz"  )
-
-		# (pow(hz,1/power)/pow(SR/2,1/power)) 
-		
-		# print QtGui.QWidget.mapFromGlobal (self, QtGui.QCursor.pos())
-		# transformedindex=int((pow(x,power)/pow(datalen/2,power))*datalen/2 )
-		
-
+			qp.drawText( QtCore.QPointF(w-64, 40) , str(freq)+" hz"  ) #draw what frequency is under the mouse
 
 			
 	def drawLines(self, qp):
@@ -288,7 +284,7 @@ class SpectrumWidget(QtGui.QWidget):
 		h = float(size.height())
 		
 		apen=QtGui.QPen()
-		# apen.setWidthF(w/(datalen/2)) #setting width doesn't seem to work
+		apen.setWidthF(((w)/(datalen/2)  ) + 200*(1/w))
 		apen.setColor(QtGui.QColor(150,200,100))
 		qp.setPen(apen)
 
@@ -296,21 +292,20 @@ class SpectrumWidget(QtGui.QWidget):
 		if phasetext == "none":
 			for i in xrange(0,datalen/2):
 				x=float(i)
-				transformedindex=int((pow(x,power)/pow(datalen/2,power))*datalen/2 )
+				transformedindex=int((pow(x,power)/pow(datalen/2,power))*datalen/2 ) #index for FFT array, compensated for scale slider
 				p1=QtCore.QPointF((x/(datalen/2))*w,h)
 				p2=QtCore.QPointF((x/(datalen/2))*w,h-(maxarray[transformedindex]/80)*h)
 				qp.drawLine(p1,p2)
 			
 		elif phasetext == "magnified average":
 		
-			total=0
+			total=0 #average all the phases of all frequencies and make the average larger
 			for i in xrange(0,datalen/2):
 				total+=phaarray[i]
 			total=(total)*4/datalen
-		
-			# apen.setWidthF(w*4/(datalen))
-			# qp.setPen(apen)
-			qp.setPen(QtGui.QColor(150-(total*1),200-(total*8),100-(total*8)))
+			
+			apen.setColor(QtGui.QColor(150-(total*1),200-(total*8),100-(total*8)))
+			qp.setPen(apen)
 		
 			for i in xrange(0,datalen/2):
 				x=float(i)
@@ -323,7 +318,8 @@ class SpectrumWidget(QtGui.QWidget):
 			for i in xrange(0,datalen/2):
 				x=float(i)
 				transformedindex=int((pow(x,power)/pow(datalen/2,power))*datalen/2 )
-				qp.setPen(QtGui.QColor(150-(phaarray[transformedindex]*1),200-(phaarray[transformedindex]*8),100-(phaarray[transformedindex]*8)))
+				apen.setColor(QtGui.QColor(150-(phaarray[transformedindex]*1),200-(phaarray[transformedindex]*8),100-(phaarray[transformedindex]*8))) #phase colours
+				qp.setPen(apen)
 				p1=QtCore.QPointF((x/(datalen/2))*w,h)
 				p2=QtCore.QPointF((x/(datalen/2))*w,h-(maxarray[transformedindex]/80)*h)
 				qp.drawLine(p1,p2)
@@ -467,7 +463,7 @@ class Example(QtGui.QMainWindow):
 		toolbar2.addWidget(BalCombo)
 		toolbar2.addWidget(LinearLabel)
 		toolbar2.addWidget(powslider)
-		LogLabel = QtGui.QLabel(" Log ")
+		LogLabel = QtGui.QLabel(" Semi-Log ")
 		toolbar2.addWidget(LogLabel)
 		toolbar2.addSeparator ()
 		
